169f793a33633871f3665f9a6ef41efc92b0872e
ninja nithin <nithin3dev@gmail.com>
2026-07-03 17:00:23 +0530

    feat: add screen point -> world point & check object boundingBox is inside camera
    - fix update camera to center

--- src/game/camera.ts
+++ src/game/camera.ts
@@ -4,18 +4,21 @@ import { addVectors } from "./utils";
 type CAMERA = { TL: Vector2D, width: number, height: number };
 // TL-> top left 
 // boundingBox TL <=> TL+{width,height}
+
+
 export class Camera {
 	private cam: CAMERA;
 	constructor(cam: CAMERA) {
 		this.cam = cam;
 	}
 
+
 	moveCamera(vect: Vector2D) {
 		this.cam.TL = addVectors(this.cam.TL, vect);
 	}
 
 	updateCamera(vect: Vector2D, center: boolean = false) {
-		this.cam.TL = center ? { x: this.cam.TL.x - (this.cam.width * 0.5), y: this.cam.TL.y - (this.cam.TL.y - (this.cam.height * 0.5)) } : vect;
+		this.cam.TL = center ? { x: vect.x - (this.cam.width * 0.5), y: vect.y - (this.cam.height * 0.5) } : vect;
 	}
 
 	boundingBox(): { v1: Vector2D, v2: Vector2D } {
@@ -33,16 +36,26 @@ export class Camera {
 		// camera space -> screen space
 		return { x: cx * scaleX, y: cy * scaleY }
 	}
+
+	screen2world(v: Vector2D, screenW: number, screenH: number): Vector2D {
+		const scaleX = screenW / this.cam.width;
+		const scaleY = screenH / this.cam.height;
+
+		// screen space -> camera space
+		const cx = v.x / scaleX;
+		const cy = v.y / scaleY;
+
+		// camera space -> world space
+		return { x: cx + this.cam.TL.x, y: cy + this.cam.TL.y };
+	}
+
+	isRender(v1: Vector2D, v2: Vector2D) { // pass boundingBox of game object
+		if (v1.x < this.cam.TL.x && v2.x < this.cam.TL.x) return false;
+		if (v1.x > this.cam.TL.x + this.cam.width && v2.x > this.cam.TL.x + this.cam.width) return false;
+		if (v1.y < this.cam.TL.y && v2.y < this.cam.TL.y) return false;
+		if (v1.y > this.cam.TL.y + this.cam.width && v2.y > this.cam.TL.y + this.cam.width) return false;
+		return true;
+	}
 }
 
 
-// cam x - width
-// cam y - height
-//
-//
-//
-// .<x1 && .len <x1
-// .>x2 && .len >x2
-// .<y1 && .len <y1
-// .>y2 && .len >y2
-// 
