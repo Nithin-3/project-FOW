643161436fbba18b32c3be0f99b3893c48642d74
ninja nithin <nithin3dev@gmail.com>
2026-07-08 21:13:07 +0530

    pref: calc shadow area when origin change

--- src/game/light.ts
+++ src/game/light.ts
@@ -1,7 +1,8 @@
 import { drawShadow } from "./shape/shadow";
 import type { Color, Vector2D } from "./types";
 import { Entity } from "./Entity";
-import { addVectors } from "./utils";
+import { addVectors, subtractVectors } from "./utils";
+import { staticQuad } from "./init";
 
 
 export class Light extends Entity {
@@ -29,15 +30,22 @@ export class Light extends Entity {
 		if (loc) this.loc = loc;
 		if (radius) this.radius = radius;
 		this._updateBounds();
+		this.render()
 	}
 
 	move(dir: Vector2D) {
 		this.loc = addVectors(this.loc, dir);
 		this._updateBounds();
-	}
-	static drawshadow(ctx: OffscreenCanvasRenderingContext2D, origin: Vector2D, radius: number, polygons: Polygon[], fillcolor?: Color) {
-		for (const poly of polygons)
-			drawShadow(ctx, origin, radius, poly, fillcolor)
+		this.render()
 	}
 
+	private render() {
+		const box = this.boundingBox();
+		this.ctx.clearRect(0, 0, this.texture.width, this.texture.height);
+		staticQuad.getBB(box).forEach(o => {
+			const obBox = o.boundingBox()
+			if (!Entity.isRender(obBox, box)) return;
+			drawShadow(this.ctx, { x: this.radius, y: this.radius }, this.radius, o.localPoints(subtractVectors(obBox.v1, box.v1), subtractVectors(obBox.v2, box.v2)), "#fff" as Color);
+		})
+	}
 }
